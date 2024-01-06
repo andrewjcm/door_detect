ba63155862b4fd21548a8fd9c9098e0bd1b99911
Andrew Metzgus <andrew@metzg.us>
2024-01-06 00:43:12 -0800

    update pushed failure message

--- services/notification.py
+++ services/notification.py
@@ -5,4 +5,4 @@ def send_pushed_notification(message):
     pushed = PushedApiRequest()
     pushed.send_notification(message)
     if not pushed.success:
-        print("Error sending message")
+        print("Error sending message: ", pushed.response.text)
