ea1badc4d25fab1d92683d479014cae8f110556a
Andrew Metzgus <andrew@metzg.us>
2024-01-06 00:45:24 -0800

    update pushed type to channel

--- services/pushed_co/schema.py
+++ services/pushed_co/schema.py
@@ -4,7 +4,8 @@ from config import settings
 class PushedRequest:
     app_key = settings.PUSHED_APP_KEY
     app_secret = settings.PUSHED_SECRET
-    target_type = "app"
+    target_type = "channel"
+    target_alias = "jMlKqi"
 
     def __init__(self, content=""):
         self.content = content
