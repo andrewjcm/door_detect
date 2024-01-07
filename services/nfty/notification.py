181ed360c46133c3a08fab88cbd14ef9926b1cc8
Andrew Metzgus <andrew@metzg.us>
2024-01-07 11:30:16 -0800

    update nfty url

--- services/nfty/notification.py
+++ services/nfty/notification.py
@@ -3,7 +3,7 @@ from config import settings
 
 
 class NftyApiRequest:
-    url = settings.NFTY_URL + "/" if settings.NFTY_URL[-1] != "/" else "" + settings.NFTY_TOPIC_ID
+    url = settings.NFTY_URL + "/" + settings.NFTY_TOPIC_ID
     status_code = None
     response = None
 
