500a274411c27c552121b4a48f0afc0cad84f186
Andrew Metzgus <andrew@metzg.us>
2024-01-08 10:46:45 -0800

    add systemd logger

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ from services.notification import send_notification
 def main():
     pin_reader = PinReader()
     while True:
-        time.sleep(.2)
         pin_reader.update_state()
         if pin_reader.state_changed:
             send_notification(pin_reader.state_message)
         elif pin_reader.is_open_time_elapsed(incremental_minutes=30):
             send_notification(pin_reader.state_time_elapsed_message)
+        time.sleep(.2)
 
 
 if __name__ == "__main__":
